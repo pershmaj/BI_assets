7c25fc6079f938fd783d0b94e4be59f2c0930623
Dmitrii Averin <averingd@gmail.com>
2021-03-19 00:07:25 +0100

    multer for asset upload

--- backend/routes.js
+++ backend/routes.js
@@ -5,6 +5,18 @@ const like = require('./controllers/like.controller');
 const middleware = require("./middleware");
 
 const router = express.Router();
+var multer  = require('multer');
+var uploadPhoto = multer({ dest: './public/assets/'});
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/assets/')
+    },
+    filename: function (req, file, cb) {
+      var filename = (new Date()).getTime().toString(36) + Math.random().toString(36).slice(2);
+      cb(null, filename);
+    }
+  });
+var uploadFile = multer({ storage: storage });
 
 router.post('/login', user.login);
 
