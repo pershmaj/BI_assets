c663240b7357fa3f982649cca87ea0b39b51fed3
Dmitrii Averin <averingd@gmail.com>
2021-03-18 20:53:15 +0100

    add element-plus

--- search-app-vue/src/main.ts
+++ search-app-vue/src/main.ts
@@ -4,9 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import ElementPlus from 'element-plus';
 
 createApp(App)
+    .use(ElementPlus)
     .use(store)
     .use(router)
     .mount('#app')
