bca1d10fc30d1062db53b6bb96800f53d179db77
Dmitrii Averin <averingd@gmail.com>
2021-03-19 13:06:59 +0100

    element plus css import

--- search-app-vue/src/main.ts
+++ search-app-vue/src/main.ts
@@ -6,6 +6,8 @@ import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus';
 
+import 'element-plus/lib/theme-chalk/index.css'
+
 createApp(App)
     .use(ElementPlus)
     .use(store)
