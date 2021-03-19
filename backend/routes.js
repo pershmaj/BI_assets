4e38f8cd035091adfc178885017023fd83c0ae1f
Dmitrii Averin <averingd@gmail.com>
2021-03-19 01:29:09 +0100

    router update

--- backend/routes.js
+++ backend/routes.js
@@ -19,5 +19,13 @@ var storage = multer.diskStorage({
 var uploadFile = multer({ storage: storage });
 
 router.post('/login', user.login);
+router.post('/registration', user.registration);
+
+router.get('/asset', middleware.isAuth, asset.getAssets);
+router.post('/asset', middleware.isAuth, uploadPhoto.single('asset'), asset.createAsset);
+router.put('/asset/:id', middleware.isAuth, uploadPhoto.single('asset'), asset.updateAsset);
+router.delete('/asset/:id', middleware.isAuth, asset.deleteAsset);
+
+router.get('/like/:user_id/:asset_id', middleware.isAuth, like.doLike);
 
 module.exports = router;
