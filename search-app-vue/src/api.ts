b58412c19e38568ac71aea7eb9a1af1ab124b9dd
Dmitrii Averin <averingd@gmail.com>
2021-03-22 13:02:23 +0100

    added deleteAsset

--- search-app-vue/src/api.ts
+++ search-app-vue/src/api.ts
@@ -4,6 +4,7 @@ const api =  {
         registration: '/api/registration',
         getAssets: '/api/asset',
         updateAsset:(assetid: number) => `/api/asset/${assetid}`, 
+        deleteAsset:(assetid: number) => `/api/asset/${assetid}`,
         like: (userid: number, assetid: number) => `/api/like/${userid}/${assetid}`,
 
         
