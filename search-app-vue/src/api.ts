69f7ebf1ea1ab135f1465e40552a35f6f392831c
Dmitrii Averin <averingd@gmail.com>
2021-03-23 14:46:26 +0100

    added updateUser and createAsset routes

--- search-app-vue/src/api.ts
+++ search-app-vue/src/api.ts
@@ -2,7 +2,9 @@ const api =  {
     urls: {
         login: '/api/login',
         registration: '/api/registration',
+        updateUser: (id: number) => `/api/user/${id}`,
         getAssets: '/api/asset',
+        createAsset: '/api/asset',
         updateAsset:(assetid: number) => `/api/asset/${assetid}`, 
         deleteAsset:(assetid: number) => `/api/asset/${assetid}`,
         like: (userid: number, assetid: number) => `/api/like/${userid}/${assetid}`,
